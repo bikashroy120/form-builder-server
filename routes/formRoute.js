b1575353e2012c0529c4bceba0ad5b190e558021
bikashroy120 <bikashroydt@gmail.com>
2024-07-27 21:43:26 +0600

    add

--- routes/formRoute.js
+++ routes/formRoute.js
@@ -4,11 +4,15 @@ import {
   createForm,
   getFormByIdFunction,
   getFormFunction,
+  updateFormFunction,
+  updateFormPublishFunction,
 } from "../controllor/formControllor.js";
 const router = express.Router();
 
 router.post("/create", isAuthenticated, createForm);
 router.get("/get", getFormFunction);
 router.get("/get/:id", getFormByIdFunction);
+router.patch("/update/:id", isAuthenticated, updateFormFunction);
+router.patch("/publish/:id", isAuthenticated, updateFormPublishFunction);
 
 export default router;
